84487812d0216ddf313877bd158d00dd9caf4949
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2025-02-26 10:05:26 -0500

    Add optional type annotations

--- src/csv-array-to-json.ts
+++ src/csv-array-to-json.ts
@@ -1,10 +1,10 @@
 export default function csvArrayToJson(rows: string[][] = []): object[] {
 	const result: object[] = [];
 
-	const length = rows.length;
-	for (let index = 1; index < length; index++) {
-		const cols = rows[index].reduce((acc, cur, idx) => {
-			const key = rows[0][idx];
+	const length: number = rows.length;
+	for (let index: number = 1; index < length; index++) {
+		const cols = rows[index].reduce((acc: object, cur: string, idx: number): object => {
+			const key: string = rows[0][idx];
 			if (key) {
 				acc[key] = cur;
 			}
--- src/csv-array-to-string.ts
+++ src/csv-array-to-string.ts
@@ -1,8 +1,6 @@
-/* BOILERPLATE */
-
 export default function csvArrayToString(rows: string[][]): string {
-	return rows.map((cols) => {
-		return cols.map((col) => {
+	return rows.map((cols: string[]) => {
+		return cols.map((col: string) => {
 			col = col.trim().replaceAll('"', '""');
 			if (col.includes('\n') || col.includes(',')) {
 				col = `"${col}"`;
