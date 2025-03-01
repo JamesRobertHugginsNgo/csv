11c6ee01c43a1a22c2531f6750ab1052a24e45d5
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2025-02-28 22:27:09 -0500

    Update code display code

--- sample/code-display.js
+++ sample/code-display.js
@@ -8,8 +8,20 @@ export default class CodeDisplay {
 	}
 
 	insert(title, description, code, ...args) {
+		const display = (result) => {
+			this.#containerEl.insertAdjacentHTML('beforeend', `<h2>${title}</h2><p>${description}</p><h3>Code</h3><pre><code>${jsBeautify(code.toString())}</code></pre><h3>Return Value</h3><pre><code>${typeof result === 'object' ? JSON.stringify(result, null, '\t') : result}</code></pre>`);
+		};
+
 		const result = code(...args);
-		this.#containerEl.insertAdjacentHTML('beforeend', `<h2>${title}</h2><p>${description}</p><h3>Code</h3><pre><code>${jsBeautify(code.toString())}</code></pre><h3>Return Value</h3><pre><code>${typeof result === 'object' ? JSON.stringify(result, null, '\t') : result}</code></pre>`);
+
+		if (result instanceof Promise) {
+			return result.then((resolveData) => {
+				display(resolveData);
+				return resolveData;
+			});
+		}
+
+		display(result);
 		return result;
 	}
 }
